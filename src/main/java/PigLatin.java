a7b561e60102cd1c90dba19b58baafb17f32a11c
Hombobow <122179708+Hombobow@users.noreply.github.com>
2024-01-17 09:04:48 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -30,7 +30,11 @@ public class PigLatin {
     public int findFirstVowel(String sWord) {
         //precondition: sWord is a valid String of length greater than 0.
         //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    // your code goes here
+	for (int i = 0; i < sWord.length(); i++) {
+		if (sWord.charAt(i) == 'a' || sWord.charAt(i) == 'e' || sWord.charAt(i) == 'i' || sWord.charAt(i) == 'o' || sWord.charAt(i) == 'u') {
+			return i;
+		}
+	}
         return -1;
     }
 
@@ -38,11 +42,12 @@ public class PigLatin {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
         // more code should go here
-	    if(findFirstVowel(sWord) == -1) {
-		    return sWord + "ay";
-	    }
-	    else {
+	
+	if(findFirstVowel(sWord) == -1) {
+		return sWord + "ay";
+	}
+	else {
 		return "ERROR!";
-	    }
+	}
     }
 }//end PigLatin class
